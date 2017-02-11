ea61caf34559d197b377f6a18bd83424ef26d9b1
Rubén López Gomez <me@rubeniskov.com>
2017-02-11 01:51:04 +0100

    update: init database scripts

--- updates/0.0.1-admins.js
+++ updates/0.0.1-admins.js
@@ -1,5 +1,10 @@
 exports.create = {
 	User: [
-		{ 'name.full': 'Paloma Sánchez Fernández', email: process.env.APP_ADMIN_USER || 'admin@admin.com', password: process.env.APP_ADMIN_PASS || 'admin', isAdmin: true }
+		{
+			'name.full': 'Paloma Sánchez Fernández',
+			email: process.env.APP_ADMIN_USER || 'admin@admin.com',
+			password: process.env.APP_ADMIN_PASS || 'admin',
+			isAdmin: true
+		}
 	]
 };
---
+++ updates/0.0.3-slider-gallery-items.js
@@ -0,0 +1,16 @@
+exports.create = {
+	SliderGalleryItem: [
+		{
+			"name": "Slide 1",
+			"header": "Header text",
+			"subtitle": "subtitle tesxt",
+			"title": "Title Text",
+			"image": {
+				"filename": "5oCJ3Xmq4UI80S14.jpg",
+				"size": 56854,
+				"mimetype": "image/jpeg"
+			},
+			"state": "published"
+		}
+	]
+};
